8619b8d1663360011a9b2c84f5dc442bb30106a8
DudipallaTejaswi <tejaswidudipalla@anarghyacomm.in>
2024-07-24 17:49:05 +0530

    Delete BankLegalproject directory

--- BankLegalproject/src/main/java/com/anarghya/bank/repository/PropertyDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anarghya.bank.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anarghya.bank.entity.PropertyDetailsEntity;
-
-public interface PropertyDetailsRepository extends JpaRepository<PropertyDetailsEntity, Long> {
-
-}
