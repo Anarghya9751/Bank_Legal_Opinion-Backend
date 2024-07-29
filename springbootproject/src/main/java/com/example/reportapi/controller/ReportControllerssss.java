bbb2aa381ebd7f07523199b0cc7bd75668bdeace
shaikafrid7 <shaikafrid@anarghyacomm.in>
2024-07-29 16:10:50 +0530

    update1

--- springbootproject/src/main/java/com/example/reportapi/controller/ReportControllerssss.java
+++ springbootproject/src/main/java/com/example/reportapi/controller/ReportControllerssss.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/reports")
-public class ReportController {
+public class ReportControllerssss {
 
     @Autowired
     private ReportServiceimp reportService;
